c59f154f83c6b617dc2c248ee8e9710842483a9c
LpxsBr <anselmolopes.an@gmail.com>
2026-08-11 11:22:53 -0300

    fix2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 // Reexport the native module. On web, it will be resolved to WalrusMemoryTrackerModule.web.ts
 // and on native platforms to WalrusMemoryTrackerModule.ts
-export { default } from './WalrusMemoryTrackerModule';
-export * from './WalrusMemoryTracker.types';
+import { NativeModule, requireNativeModule } from 'expo';
+import type { MemoryInfo } from './WalrusMemoryTracker.types';
+
+export declare class WalrusMemoryTrackerModule
+  extends NativeModule<{}> {
+  hello(): string;
+  getMemoryInfo(): Promise<MemoryInfo>;
+}
+
+export default requireNativeModule<WalrusMemoryTrackerModule>(
+  'WalrusMemoryTracker'
+);
